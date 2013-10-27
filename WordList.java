c368234c029cd6e2fc1bb8fe1c9ee37058cbb0ed
Joe McCully <jmccully88@yahoo.com>
2013-10-26 22:53:24 -0700

    best version so far

--- WordList.java
+++ WordList.java
@@ -3,20 +3,29 @@ import java.io.*;
 public class WordList{
   public WordList(){
     LinkedList<Word> words = new LinkedList<Word>();
-  }  
+  }
+  String s1;
   LinkedList<Word> words = new LinkedList<Word>();
+  ListIterator<Word> it = words.listIterator();
+  boolean added = false;
   public LinkedList<Word> scanFile() throws FileNotFoundException{
     Scanner in = new Scanner(new File("emma.txt"));
     in.useDelimiter("[^a-zA-Z]+");
     while (in.hasNext()){
-      String s = in.next().toLowerCase();
-      Word newWord = new Word(s,1);
-
-      words.add(newWord);
+      String s1 = in.next().toLowerCase();
+      Word newWord = new Word(s1,1);
+      it = words.listIterator(0);
+      if(words.size() == 0){
+        words.add(newWord);
       }
+      else{
+          words.add(newWord);
+      }
+    }
     in.close();
     return words;
-    }
+  }
+
 
   class Word{ 
     String s;
@@ -29,8 +38,14 @@ public class WordList{
       s = ins;
       count = incount;
     }
-      public String toString(){
+    public void incCount(){
+    count++;
+    }
+    public String getString(){
+      return s;
+    }
+    public String toString(){
     return ("list:" + s + " count " +count);
-  }
+    }
   }
 }
